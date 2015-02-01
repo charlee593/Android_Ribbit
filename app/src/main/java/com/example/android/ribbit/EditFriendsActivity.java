49c36de22f696f1bce624d22bfc9d923d65327de
charlee593 <charlee593@gmail.com>
2015-02-01 02:00:49 -0500

    Add add friends and remove friend implemention

--- app/src/main/java/com/example/android/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/example/android/ribbit/EditFriendsActivity.java
@@ -3,14 +3,16 @@ package com.example.android.ribbit;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -18,16 +20,22 @@ import java.util.List;
 public class EditFriendsActivity extends ListActivity {
 
     private List<ParseUser> users;
+    private ParseUser mCurrentUser;
+    private ParseRelation<ParseUser> friendsRelation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        friendsRelation = mCurrentUser.getRelation("friendsRelation");
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending("username");
@@ -47,6 +55,7 @@ public class EditFriendsActivity extends ListActivity {
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this,
                             android.R.layout.simple_list_item_checked, usernames);
                     setListAdapter(adapter);
+                    addCheckMarks();
                 }
                 else{
                     Log.i(EditFriendsActivity.class.getSimpleName(), "Parse query error");
@@ -55,25 +64,44 @@ public class EditFriendsActivity extends ListActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_friends, menu);
-        return true;
+    private void addCheckMarks() {
+        friendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+                if(e == null){
+                    for(int i = 0; i < users.size(); i++){
+                        ParseUser user = users.get(i);
+                        for(ParseUser user2 : parseUsers){
+                            if(user2.getObjectId().equals(user.getObjectId())){
+                                getListView().setItemChecked(i, true);
+                            }
+                        }
+                    }
+                }
+                else{
+                    Log.i(EditFriendsActivity.class.getSimpleName(), "AddCheckmarks error");
+                }
+            }
+        });
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if(getListView().isItemChecked(position)){
+            friendsRelation.add(users.get(position));
         }
+        else{
+           friendsRelation.remove(users.get(position));
+        }
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.i(EditFriendsActivity.class.getSimpleName(), "saving new friend error");
+                }
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
     }
 }
